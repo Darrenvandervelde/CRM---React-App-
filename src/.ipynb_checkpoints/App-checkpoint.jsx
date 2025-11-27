0b3ba1f9e3fb0bc55fdf077bb7471b5a4602e5e4
Darren van der Velde <darrenvandervelde@gmail.com>
2025-11-27 13:27:39 +0000

    update

---
+++ src/.ipynb_checkpoints/App-checkpoint.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <h2>Welcome to the CRM </h2>
+      </body>
+    </div>
+  );
+}
+
+export default App;
